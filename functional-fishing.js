7e925e1a8a8f98033277d4b33eb9d9e2611898c9
mest0006 <46438846+mest0006@users.noreply.github.com>
2020-02-12 10:23:28 -0500

    finished functional fishing

--- functional-fishing.js
+++ functional-fishing.js
@@ -1,7 +1,6 @@
 /**
  * Function Fishing
  * A simple fishing game
- *
  * Rules:
  * To Eat: Must have at least 1 fish and a fire
  * To Make Fire: Must have a least 1 log of wood
@@ -10,29 +9,83 @@
  * To Find Bait: The fire must be out.
  */
 
+
+
+const settings = {
+  fireWood: 1,
+  fishingBait: 1,
+  eating: 1
+}
+
+
 /**
  * The game object stores the current game status
  */
 const game = {
+  fire: false,
+  wood: 0,
+  fish: 1,
+  bait: 0,
+
 
 }
+
 
 /**
  * fire
  * To start a fire:
  *    The fire must be out
  *    There must be at least 1 piece of wood
- *
+ *                        
  * To stop a fire:
  *    The fire must be going
  */
 
+function fire () {
+  if (game.fire) {
+
+    game.fire = false
+    return 'You have put out the fire.'
+
+  } else if (game.wood > 0) {
+    game.wood--
+    game.fire = true
+    return 'You have started the fire.'
+  } else {
+    return 'You do not have enough wood.'
+  }
+}
 /**
  * fish
  * To go fishing:
  *    The fire must be out
  *    There must be at least 1 piece of bait
  */
+/**
+ * check if game.fire is false
+ * if game.fire is false
+ * check if  there is bait 
+ */
+
+function fish () {
+  if (game.fire) {
+    return 'The fire must be out'
+  } else {
+    if (game.bait >= 0) {
+      game.fish++
+      return 'You have one fish'
+    }
+
+  }
+
+}
+
+
+
+
+
+
+
 
 /**
  * bait
@@ -40,11 +93,36 @@ const game = {
  *    The fire must be out
  */
 
+
+function bait () {
+  if (game.fire) {
+
+    return ' you have put out the fire'
+  } else {
+
+    game.bait++
+    return 'you have 1 piece of bait'
+  }
+
+}
 /**
  * wood
  * To search for wood:
  *    The first must be out
  */
+function wood () {
+  if (game.wood) {
+    return 'you do not have any wood'
+  } else {
+    game.wood++
+    return ' you have 1 piece of wood'
+  }
+
+
+}
+
+
+
 
 /**
  * eat
@@ -52,11 +130,43 @@ const game = {
  *    There first must be going
  *    There must be at least 1 fish
  */
-
+function eat () {
+  if (game.fire === true) {
+    if (game.fish >= settings.eating) {
+      game.fish--
+      return 'You have eaten a fish'
+    } else {
+      return 'You do not  have any fish'
+    }
+  } else {
+    return `the fire must be burning`
+  }
+}
 /**
  * inventory
  * Shows the players current inventory
  */
+
+
+function inventory () {
+  const response = []
+  response.push('INVENTORY:\n')
+  for (const item in game) {
+    if (item === 'fire') {
+      if (game[item]) {
+        response.push('The fire is burning.')
+      } else {
+        response.push('The fire is not burning.')
+      }
+    } else {
+      response.push(`${item} ${game[item]}\n`)
+    }
+  }
+  return response.join(' ')
+}
+
+
+
 
 /**
  * help
